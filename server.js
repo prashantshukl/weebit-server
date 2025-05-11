3425b5d6df6e1074eeeaf51a6a888e4305645c01
Prashant Shukla <prashantshukla1029@gmail.com>
2025-05-11 19:52:35 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+
+
+//App config
+const app = express();
+const port = process.env.PORT || 4000;
+
+//middlewares
+app.use(express.json());
+app.use(cors())
+
+app.get('/', (req, res) => res.send('API Working'));
+
+app.listen(port, () => {
+   console.log(`server is running at port : ${port}`);
+});
